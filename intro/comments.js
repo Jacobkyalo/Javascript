6281d5952655283f7826fa514161640baf42a3c6
Jacob <jacobkyalo43@gmail.com>
2022-05-23 09:55:01 +0300

    first commit

---
+++ intro/comments.js
@@ -0,0 +1,12 @@
+// JS comments can be written with (//) or between (/**/)
+// e.g
+
+function comments() {
+  // This is a single line comment
+  /*
+  This is also a comment,
+  a multiline comment
+  These comments are mostly used for formal
+  documentation
+  */
+}
---
+++ intro/intro.js
@@ -0,0 +1,43 @@
+// JS is the world most popular programming language.
+// JS is the programming language for the web.
+// JS has had many versions as of today for example
+// JS accepts both single and double quotes
+// JS was invented by Brendan Eich in 1995 and became an ECMA standard in 1997
+// ECMAScript is the official name of the language
+// ECMAScript versions have been abbreviated to ES1, ES2, ES3, ES5, and ES6.
+//JS and Java are completely different languages
+
+// 1. The Original JavaScript ES1 ES2 ES3 (1997-1999)
+// 2. The First Main Revision ES5 (2009)
+// 3. The Second Revision ES6 (2015)
+// 4. The Yearly Additions (2016, 2017, 2018)
+
+//JS can do the following
+// 1. JS can change HTML content
+// 2. JS can change HTML attribute values
+// 3. JS can change HTML styles (CSS)
+// 4. JS can hide HTML elements
+// 5. JS can show HTML elements
+
+// In HTML JS code is inserted between <script> and </script> tags
+// NO need for type attribute
+// Placing scripts at the bottom of the <body> element improves the display speed, because script interpretation slows down the display.
+function myFunction() {
+  var x = 10;
+  var y = 20;
+  var z = (x + y) ** 2;
+  document.getElementById("demo").innerHTML = z;
+  console.log("Done");
+  //console.log("one" + " two");
+  //alert("hello");
+}
+
+//document.write(3 + 5);
+
+//OUTPUT with JS
+//JavaScript can "display" data in different ways:
+
+//Writing into an HTML element, using innerHTML.
+//Writing into the HTML output using document.write().
+//Writing into an alert box, using window.alert().
+//Writing into the browser console, using console.log().
