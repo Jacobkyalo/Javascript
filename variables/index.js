4bc169aedd9d8df3b3769e7284261b5256f22b78
Jacob <jacobkyalo43@gmail.com>
2022-05-23 10:43:17 +0300

    variable declaraction using var,let and const

---
+++ variables/index.js
@@ -0,0 +1,37 @@
+// variables are containers for storing values
+// variables can be declared using: let, var, const keywords
+var x = 5;
+var y = 20;
+var z = x + y;
+
+console.log(z);
+//let
+let x1 = 50;
+let y1 = 20;
+let z1 = x1 + y1;
+
+console.log(z1);
+//const
+const x2 = 40;
+const y2 = 20;
+const Y2 = 40; // Different from y2(case sensitive)
+const z2 = x2 + Y2;
+
+console.log(z2);
+//For strings
+
+let name = "Jacob";
+let commet = "I love JavaScript";
+const language = "Python";
+let car;
+
+console.log(name);
+console.log(commet + " and " + language);
+console.log(car); // returns undefined
+console.log(3 + 6 + "7");
+
+//area of a circle project
+const PI = 3.14;
+const radius = 10;
+const area = PI * radius * radius;
+console.log("The area of the circle is:", area);
