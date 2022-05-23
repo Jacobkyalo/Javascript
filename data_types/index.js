4bb87e47a6479a440110caab3f3b7cbc8c7849ca
Jacob <jacobkyalo43@gmail.com>
2022-05-23 16:22:26 +0300

    data types

---
+++ data_types/index.js
@@ -0,0 +1,20 @@
+//JS data types can be numbers, strings, objects, arrays e.t.c
+
+let age = 30; //number
+let name = "Joseph"; //string
+let cars = ["Volvo", "Ford", "Range Rover"]; //array
+let person = { firstName: "John", lastName: "Doe", age: 40 }; //object
+
+//strings can use single or double quotes
+
+//Boolean values
+let isMale = true;
+let isTall = false;
+
+console.log(isMale == isTall); //returns false
+console.log(isMale !== isTall); //returns true
+
+//typeof operator
+console.log(typeof isTall); //returns boolean
+let x = "";
+console.log(typeof x); //returns string
