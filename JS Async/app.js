f0af0415f783e1b8ccee3e3b18e78ca584a8458e
Jacob <jacobkyalo43@gmail.com>
2022-06-08 18:10:57 +0300

    js async and await

--- JS Async/app.js
+++ JS Async/app.js
@@ -27,8 +27,37 @@ let promise1 = new Promise((req, res) => {
   setTimeout(() => req("I love JS"), 2000);
 });
 promise1.then((value) => {
-  document.getElementById("democ").innerHTML = value;
+  document.getElementById("demo").innerHTML = value;
   (error) => {
     console.log(error);
   };
 });
+
+//Async-await
+
+(async function myName2() {
+  return "Hello Jack";
+})().then(
+  function (value) {
+    console.log(value);
+  },
+  function (err) {
+    console.log(err);
+  }
+);
+
+//async and DOM
+(async function iLoveAsync() {
+  let newPromise = new Promise((req, res) => {
+    req("I Love Async and Await!");
+  });
+  document.getElementById("head").innerHTML = await newPromise;
+})();
+
+(async function iLoveDate() {
+  let date = new Promise((req, res) => {
+    req(new Date());
+    res("error");
+  });
+  document.getElementById("head2").innerHTML = await date;
+})();
